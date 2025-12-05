9bfff0c4c3721d5477e6e16f2d42f93b4564491b
wyenox <227889946+wyenox@users.noreply.github.com>
2025-12-05 11:28:28 -0300

    chat widget uses little change

--- moly-kit/src/widgets/chat.rs
+++ moly-kit/src/widgets/chat.rs
@@ -2,24 +2,9 @@ use makepad_widgets::*;
 use std::cell::{Ref, RefMut};
 use std::sync::{Arc, Mutex};
 
-use crate::{
-    ai_kit::{
-        controllers::chat::{
-            ChatController, ChatControllerPlugin, ChatControllerPluginRegistrationId, ChatState,
-            ChatStateMutation, ChatTask,
-        },
-        mcp::mcp_manager::display_name_from_namespaced,
-        protocol::*,
-        utils::vec::VecMutation,
-    },
-    utils::makepad::events::EventExt,
-    widgets::{
-        messages::{MessagesAction, MessagesRef, MessagesWidgetExt},
-        moly_modal::MolyModalWidgetExt,
-        prompt_input::{PromptInputRef, PromptInputWidgetExt},
-        realtime::{RealtimeModalAction, RealtimeWidgetExt},
-    },
-};
+use crate::ai_kit::mcp::mcp_manager::display_name_from_namespaced;
+use crate::prelude::*;
+use crate::utils::makepad::events::EventExt;
 
 live_design!(
     use link::theme::*;
